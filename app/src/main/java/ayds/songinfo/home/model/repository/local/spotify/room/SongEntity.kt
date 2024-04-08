11c4e9e3b8b872b2230c92802c863cebcd8471c3
Francisco <franclarke00@hotmail.com>
2024-04-08 09:09:11 -0300

    Etapa 2

---
+++ app/src/main/java/ayds/songinfo/home/model/repository/local/spotify/room/SongEntity.kt
@@ -0,0 +1,17 @@
+package ayds.songinfo.home.model.repository.local.spotify.room
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class SongEntity(
+    @PrimaryKey val id: String,
+    val term: String,
+    val songName: String,
+    val artistName: String,
+    val albumName: String,
+    val releaseDate: String,
+    val releaseDatePrecision: String,
+    val spotifyUrl: String,
+    val imageUrl: String,
+)
